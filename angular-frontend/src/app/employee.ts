f75fece6eebce498c03a3d413a386502f82ad1f7
HARITHA MANOHARAN <103134649+harithamanoharan10@users.noreply.github.com>
2022-04-07 12:30:52 +0530

    Add files via upload

---
+++ angular-frontend/src/app/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    empId: number;
+    fullName: string;
+    emailId: string;
+    address: string;
+    manager: string;
+    mobile: string;
+    project: string;
+    
+}
